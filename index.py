01a2f5792ddd7c36bc1045192c7b1a1390446e91
Zied Guesmi <Guesmy.Zied@gmail.com>
2017-11-27 01:31:07 +0100

    seperate the two modes of the assistant

--- index.py
+++ index.py
@@ -1,56 +1,11 @@
-#!/usr/bin/python3
-
-import time
 import RPi.GPIO as GPIO
-
-# Get distance between the user and the nearest obstacle
-def getDistance(GPIO_TRIGGER, GPIO_ECHO):
-    #Send 10us pulse to trigger
-    GPIO.output(GPIO_TRIGGER, True)
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
-    start = time.time()
-    while GPIO.input(GPIO_ECHO) == 0:
-        start = time.time()
-    while GPIO.input(GPIO_ECHO) == 1:
-        stop = time.time()
-    #Calculate pulse length
-    duration = stop-start
-    #Distance pulse travelled in that time is time
-    #multiplied by the speed of sound (cm/s)
-    distance = duration * 34000
-    #That was the distance there and back so halve the value
-    distance = distance / 2
-    return distance
-
-# Play warnings according to distance and language
-def play_sound(distance, language):
-    distance = str(distance)
-    pygame.mixer.init()
-    pygame.mixer.music.set_volume(1.0)
-    pygame.mixer.music.load("resources/sounds/" + language + "/warning" + str(distance) + ".mp3")
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() is True:
-        continue
-    pygame.quit()
-
-# play warnings
-def warn(distance):
-    try:
-        if distance < 100:
-            print("you should stop")
-            play_sound(100, language)
-        elif distance < 200:
-            print("The obstacle is 2 meters ahead")
-            play_sound(200, language)
-        elif distance < 400:
-            print("There is an obstacle after 4 meters")
-            play_sound(400, language)
-    except:
-        print("=> Error playing sound")
+import time
+import os
+from picamera import PiCamera
+from camera_mode import camera_mode
+from obstacle_detection_mode import obstacle_detection_mode
 
 
-# Choose language of warnings
 language = "EN"
 
 # Use BCM GPIO references instead of physical pin numbers
@@ -59,6 +14,8 @@ GPIO.setmode(GPIO.BCM)
 # Define GPIO to use on Pi
 GPIO_TRIGGER = 23
 GPIO_ECHO = 24
+GPIO_MODE_BUTTON = 16
+GPIO_CAPTURE_BUTTON = 25
 
 # Set pins as output and input
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)  # Trigger
@@ -67,11 +24,27 @@ GPIO.setup(GPIO_ECHO, GPIO.IN)      # Echo
 # Set trigger to False (Low)
 GPIO.output(GPIO_TRIGGER, False)
 
+GPIO.setup(GPIO_MODE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(GPIO_CAPTURE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 # Allow module to settle
 time.sleep(0.1)
 
+GPIO.add_event_detect(GPIO_MODE_BUTTON, GPIO.BOTH)
+GPIO.add_event_detect(GPIO_CAPTURE_BUTTON, GPIO.FALLING)
+
+os.system("amixer set PCM -- 100%")
+camera = PiCamera()
+
 while True:
-    distance = getDistance(GPIO_TRIGGER, GPIO_ECHO)
-    print("Distance : %.1f" % distance)
-    warn(distance)
+    print("====> Obstacle detection mode")
+    obstacle_detection_mode(GPIO_TRIGGER, GPIO_ECHO, language)
+
+    if GPIO.event_detected(GPIO_MODE_BUTTON) and GPIO.input(GPIO_MODE_BUTTON) == False:
+        print("====> Camera mode")
+        while True:
+            if GPIO.event_detected(GPIO_CAPTURE_BUTTON):
+                camera_mode(camera)
+            elif GPIO.event_detected(GPIO_MODE_BUTTON) and GPIO.input(GPIO_MODE_BUTTON) == True:
+                break
     time.sleep(0.1)
---
+++ obstacle_detection_mode.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import time
+import RPi.GPIO as GPIO
+import os
+
+# Get distance between the user and the nearest obstacle
+def getDistance(GPIO_TRIGGER, GPIO_ECHO):
+    #Send 10us pulse to trigger
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+    start = time.time()
+    while GPIO.input(GPIO_ECHO) == 0:
+        start = time.time()
+    while GPIO.input(GPIO_ECHO) == 1:
+        stop = time.time()
+    #Calculate pulse length
+    duration = stop-start
+    #Distance pulse travelled in that time is time
+    #multiplied by the speed of sound (cm/s)
+    distance = duration * 34000
+    #That was the distance there and back so halve the value
+    distance = distance / 2
+    return distance
+
+# Play warnings according to distance and language
+def play_sound(distance, language):
+    os.system("mpg123 resources/sounds/" + language + "/warning" + str(distance) + ".mp3")
+
+# play warnings
+def warn(distance, language):
+    try:
+        if distance < 100:
+            print("you should stop")
+            play_sound(100, language)
+        elif distance < 200:
+            print("The obstacle is 2 meters ahead")
+            play_sound(200, language)
+        elif distance < 400:
+            print("There is an obstacle after 4 meters")
+            play_sound(400, language)
+    except:
+        print("=> Error playing sound")
+
+
+def obstacle_detection_mode(GPIO_TRIGGER, GPIO_ECHO, language):
+    distance = getDistance(GPIO_TRIGGER, GPIO_ECHO)
+    print("Distance : %.1f" % distance)
+    warn(distance, language)
