8ab17db22da6943eb757304bde271908e33797ec
Zied Guesmi <Guesmy.Zied@gmail.com>
2018-10-10 12:48:36 +0100

    refactor main

--- src/main.py
+++ src/main.py
@@ -1,60 +1,82 @@
-import RPi.GPIO as GPIO
-import time
 import os
-from image_to_speech import ImageToSpeech
+import time
+import logging
+
+import RPi.GPIO as GPIO
+
 from obstacle_detector import ObstacleDetector
+from document_reader import DocumentReader
 
-class Main(object):
-    """docstring for Main."""
-    def __init__(self, arg):
-        super(Main, self).__init__()
-        self.arg = arg
 
-    def setup(arg):
-        logging.basicConfig(filename='/tmp/blind-assistant.log', level=logging.INFO)
+class Config():
+
+    def __init__(self):
+        LANG = "EN"
+        GPIO_TRIGGER = 23
+        GPIO_ECHO = 24
+        GPIO_CHANGE_MODE_BUTTON = 16
+        GPIO_CAPTURE_IMAGE_BUTTON = 25
+        LOGFILE = '/tmp/blind-assistant.log'
+        LOGLEVEL = logging.INFO
+
+
+class Main():
+
+    config = None
+
+    def __init__(self, config):
+        config = Config()
+        self.setup()
+
+    def setup(self):
+        # set logging file and level
+        logging.basicConfig(filename=self.config.LOGFILE, level=self.config.LOGLEVEL)
+
+        # Use BCM GPIO references instead of physical pin numbers
+        GPIO.setmode(GPIO.BCM)
+
+        # Set pins as output and input
+        GPIO.setup(self.config.GPIO_TRIGGER, GPIO.OUT)  # Trigger
+        GPIO.setup(self.config.GPIO_ECHO, GPIO.IN)      # Echo
+
+        # Set trigger to False (Low)
+        GPIO.output(self.config.GPIO_TRIGGER, False)
+
+        GPIO.setup(self.config.GPIO_CHANGE_MODE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.config.GPIO_CAPTURE_IMAGE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        # Allow module to settle
+        time.sleep(0.1)
+
+        GPIO.add_event_detect(self.config.GPIO_CHANGE_MODE_BUTTON, GPIO.BOTH)
+        GPIO.add_event_detect(self.config.GPIO_CAPTURE_IMAGE_BUTTON, GPIO.FALLING)
+
+        # increase sound volume
+        os.system("amixer set PCM -- 100%")
+
+    def run(self):
+
         logging.info('Started')
-        ObstacleDetector.do_something()
+        obstacle_detector = ObstacleDetector()
+        document_reader = DocumentReader()
+
+        while True:
+            logging.info("Running obstacle detector")
+            obstacle_detector.run(self.config.GPIO_TRIGGER, self.config.GPIO_ECHO, self.config.LANG)
+
+            if GPIO.event_detected(self.config.GPIO_CHANGE_MODE_BUTTON) and GPIO.input(self.config.GPIO_CHANGE_MODE_BUTTON) == False:
+                logging.info("Running document reader")
+
+                while True:
+                    if GPIO.event_detected(self.config.GPIO_CAPTURE_IMAGE_BUTTON):
+                        document_reader.run()
+                        # camera_mode(camera) maybe u should use one camera instance
+                    elif GPIO.event_detected(self.config.GPIO_CHANGE_MODE_BUTTON) and GPIO.input(self.config.GPIO_CHANGE_MODE_BUTTON) == True:
+                        break
+
+            time.sleep(0.1)
+
         logging.info('Finished')
 
-
-language = "EN"
-
-# Use BCM GPIO references instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
-
-# Define GPIO to use on Pi
-GPIO_TRIGGER = 23
-GPIO_ECHO = 24
-GPIO_MODE_BUTTON = 16
-GPIO_CAPTURE_BUTTON = 25
-
-# Set pins as output and input
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)  # Trigger
-GPIO.setup(GPIO_ECHO, GPIO.IN)      # Echo
-
-# Set trigger to False (Low)
-GPIO.output(GPIO_TRIGGER, False)
-
-GPIO.setup(GPIO_MODE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(GPIO_CAPTURE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# Allow module to settle
-time.sleep(0.1)
-
-GPIO.add_event_detect(GPIO_MODE_BUTTON, GPIO.BOTH)
-GPIO.add_event_detect(GPIO_CAPTURE_BUTTON, GPIO.FALLING)
-
-os.system("amixer set PCM -- 100%")
-
-while True:
-    print("====> Obstacle detection mode")
-    obstacle_detection_mode(GPIO_TRIGGER, GPIO_ECHO, language)
-
-    if GPIO.event_detected(GPIO_MODE_BUTTON) and GPIO.input(GPIO_MODE_BUTTON) == False:
-        print("====> Camera mode")
-        while True:
-            if GPIO.event_detected(GPIO_CAPTURE_BUTTON):
-                camera_mode(camera)
-            elif GPIO.event_detected(GPIO_MODE_BUTTON) and GPIO.input(GPIO_MODE_BUTTON) == True:
-                break
-    time.sleep(0.1)
+if __name__ == '__main__':
+    pass
